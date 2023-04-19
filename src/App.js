755ed30a9cf2f6e699a8bf81fdce562fe137686c
marianami19 <45075042+marianami19@users.noreply.github.com>
2023-04-19 22:55:52 +0530

    toggle issue solved - final review left

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <header className="App-header">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <a className="navbar-brand" href="#">Sales App</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
